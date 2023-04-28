4faa306e5beb289cfd596b21724190c7cb182264
Vitaly <vitalic.web@gmail.com>
2023-04-28 20:28:10 +0300

    Finish usePageRequest composable

--- src/composables/usePageRequests.js
+++ src/composables/usePageRequests.js
@@ -1,15 +1,23 @@
 import {computed, reactive} from 'vue';
 
+// переменную нужно вынести наружу чтобы при каждом запросе не перезаписывалась
+// работает как общий стейт для всего приложения
+// если нужна глобальная переменная необходимо ее выносить снаружи экспорируемой функции
+const activeRequests = reactive([]);
+
 export default function usePageRequests() {
-  const activeRequests = reactive([]);
   const isLoading = computed(() => !!activeRequests.length);
 
   const makeRequest = async (url) => {
     // push the url to the activeRequests
-    const response = await fetch(url);
+    const index = activeRequests.length;
+    activeRequests[index] = url;
+    const response = await fetch(url)
+      .catch(error => alert(error)); // if failed remove the url from activeRequests and alert error
     const data = await response.json();
     // remove the url from activeRequests
-    // if failed remove the url from activeRequests and alert error
+    activeRequests.splice(index, 1);
+    return data;
   }
 
   return { isLoading, makeRequest }
